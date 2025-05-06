e502b4777b0669d80b68d469eb0e47739a489e54
Muhammad Daffa Ramadhan <amateurmdr@gmail.com>
2025-05-06 13:42:17 +0700

    add TugasDao interface with basic CRUD operations

--- app/src/main/java/com/daffa0049/todomahasiswaapp/data/TugasDao.kt
+++ app/src/main/java/com/daffa0049/todomahasiswaapp/data/TugasDao.kt
@@ -1,2 +1,19 @@
 package com.daffa0049.todomahasiswaapp.data
 
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TugasDao {
+    @Query("SELECT * FROM tugas")
+    fun getAll(): Flow<List<Tugas>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(tugas: Tugas)
+
+    @Update
+    suspend fun update(tugas: Tugas)
+
+    @Delete
+    suspend fun delete(tugas: Tugas)
+}
