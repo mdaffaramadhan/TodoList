34976cb60d9bfdf5245a2651127658292e5edbb2
Muhammad Daffa Ramadhan <amateurmdr@gmail.com>
2025-05-06 13:43:12 +0700

    add TugasDatabase singleton Room database

--- app/src/main/java/com/daffa0049/todomahasiswaapp/data/TugasDatabase.kt
+++ app/src/main/java/com/daffa0049/todomahasiswaapp/data/TugasDatabase.kt
@@ -1,4 +1,28 @@
 package com.daffa0049.todomahasiswaapp.data
 
-class TugasDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Tugas::class], version = 1, exportSchema = false)
+abstract class TugasDatabase : RoomDatabase() {
+    abstract fun tugasDao(): TugasDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: TugasDatabase? = null
+
+        fun getDatabase(context: Context): TugasDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TugasDatabase::class.java,
+                    "tugas_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
